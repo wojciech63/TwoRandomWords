64976052e5f99fce5a33997ae89e5d29bde5472a
Wojciech <wojt648@gmail.com>
2025-03-03 18:32:59 +0100

    Centered column with the word, made more room between our word and next button.

--- lib/main.dart
+++ lib/main.dart
@@ -42,17 +42,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -81,6 +84,7 @@ class BigCard extends StatelessWidget {
         child: Text(
           pair.asLowerCase,
           style: style,
+          semanticsLabel: pair.asPascalCase,
         ),
       ),
     );
